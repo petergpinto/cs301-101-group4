7c52ac4c544306999ab5ce54e3188c8ea3bb4ba7
Peter Pinto <petergpinto@verizon.net>
2021-11-18 10:01:28 -0500

    Queries for each ticker, run every 5 minutes and output data to CSV accessable by webserver

---
+++ queries/NVDAquery.sql
@@ -0,0 +1,2 @@
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+select ticker, timestamp,  avg(sentiment) from ticker_mentions where ticker="NVDA" group by UNIX_TIMESTAMP(timestamp) DIV 600;
